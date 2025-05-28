7f2882e8cb46585b2a212a39c220f26af22b85ce
polinamulyarchik <polinamularcik8@gmail.com>
2025-05-28 09:15:46 +0300

    Add files via upload

---
+++ src/main/java/com/example/watercontrol/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.watercontrol.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFBCFFFD)
+val PurpleGrey80 = Color(0xFFC2D5DC)
+val Pink80 = Color(0xFFB8CCEF)
+
+val Purple40 = Color(0xFF5076A4)
+val PurpleGrey40 = Color(0xFF5B6471)
+val Pink40 = Color(0xFF525D7D)
